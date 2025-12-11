b161b95194467ec658b3936ccd899b6c3b7c47fd
Nazi <mehdizadeh.sn@gmail.com>
2025-12-11 18:32:01 +0000

    Contract created and state variables added

--- absolute/contracts/Dex.sol
+++ absolute/contracts/Dex.sol
@@ -3,3 +3,16 @@ pragma solidity ^0.8.28;
 
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract  Dex {
+  IERC20 public immutable token0;
+  IERC20 public immutable token1;
+
+  uint public reserve0;;
+  uint public reserve1;
+
+  uint public constant K = 0;
+  uint public constant FEE = 300;
+
+  
+}
